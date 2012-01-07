1b854c2778d055d6fffe2d2d903e4f2eea30dbd0
Monica McArthur <monica@revolutionprep.com>
2012-01-06 17:08:14 -0800

    Symbol equality issue fix

--- surety.gemspec
+++ surety.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name              = 'surety'
-  s.version           = '0.1.6'
+  s.version           = '0.1.7'
   s.date              = Time.now.strftime('%Y-%m-%d')
   s.summary           = 'A lightweight guaranteed-delivery messaging system.'
   s.homepage          = ''
--- test/message_test.rb
+++ test/message_test.rb
@@ -71,7 +71,7 @@ class MessageTest < Test::Unit::TestCase
       assert_in_delta(@message.processing_started_at, Time.now, 5)
       assert_in_delta(@message.failed_at, Time.now, 5)
   end
-
+  
   def test_message_retry
     @message.begin_processing!
     @message.fail_processing!
@@ -281,52 +281,4 @@ class MessageTest < Test::Unit::TestCase
     assert message.nil?
   end
   
-  def test_needs_processing_failure_timeout_when_active_record_using_utc
-    ActiveRecord::Base.default_timezone = :utc
-    Surety::Message.new.connection.execute("truncate table messages")
-    @message1 = Surety::Message.generate_message('Message 1')
-    @message2 = Surety::Message.generate_message('Message 2')
-    ActiveRecord::Base.default_timezone = :local
-    message = Surety::Message.needs_processing.first
-    message.begin_processing!
-    assert message.present?
-    assert_message_matches(message, {
-      :message_content=>'Message 1',
-      :state => 'processing',
-      :processing_attempt_count => 1,
-      :failure_count => 0,
-      :completed_at => nil,
-      :failed_at => nil,
-      :last_exception => nil
-    })
-    assert_in_delta(message.processing_started_at, Time.now, 5)
-    @message1.reload
-    @message1.fail_processing!
-    message = Surety::Message.get_next_for_processing
-    assert message.present?
-    assert_message_matches(message, {
-      :message_content=>'Message 2',
-      :state => 'processing',
-      :processing_attempt_count => 1,
-      :failure_count => 0,
-      :completed_at => nil,
-      :failed_at => nil,
-      :last_exception => nil
-    })
-    assert_in_delta(message.processing_started_at, Time.now, 5)
-    @message1.failed_at = Time.now-40.minutes
-    @message1.save
-    message = Surety::Message.get_next_for_processing
-    assert message.present?
-    assert_message_matches(message, {
-      :message_content=>'Message 1',
-      :state => 'processing',
-      :processing_attempt_count => 2,
-      :failure_count => 1,
-      :completed_at => nil,
-      :last_exception => nil
-    })
-    assert_in_delta(message.processing_started_at, Time.now, 5)
-  end
-
 end
